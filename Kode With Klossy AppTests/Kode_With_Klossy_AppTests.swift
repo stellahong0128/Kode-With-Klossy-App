f59b7b8d373dfb4f40c3aa62ac68da18594122b6
Stella Hong <stellahong0128@gmail.com>
2020-07-15 13:26:45 -0700

    Testing

---
+++ Kode With Klossy AppTests/Kode_With_Klossy_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kode_With_Klossy_AppTests.swift
+//  Kode With Klossy AppTests
+//
+//  Created by Stella Hong on 7/15/20.
+//  Copyright © 2020 Stella Hong. All rights reserved.
+//
+
+import XCTest
+@testable import Kode_With_Klossy_App
+
+class Kode_With_Klossy_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
